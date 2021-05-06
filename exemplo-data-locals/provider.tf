61605ef6935289cd1effdac3416ac64b23cd65de
Odirlei Nascimento <odirlei.nascimento@gmail.com>
2021-05-06 02:16:02 +0000

    first commit

---
+++ exemplo-data-locals/provider.tf
@@ -0,0 +1,6 @@
+provider "google" {
+  project = "odirlei-mbaimpacta-turma04"
+  region = "us-central1"
+  zone = "us-central1-a"
+  credentials = "../odirlei-mbaimpacta-turma04-fe5e3e12c277.json"
+}
